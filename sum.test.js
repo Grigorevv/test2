2f24f74d03aa5a667574e0edb337f08a8c691e6a
Viktor Grigorev <7184243@gmail.com>
2020-10-27 15:47:30 +0300

    test 5

--- sum.test.js
+++ sum.test.js
@@ -1,9 +1,9 @@
 import { expect, test } from '@jest/globals';
 import add, { sub, mult, div } from './func.js';
 
-/*test('adds 1 + 2 to equal 3', () => {
+test('adds 1 + 2 to equal 3', () => {
   expect(add(1, 2)).toBe(3);
-});*/
+});
 
 test('sub 10 - 10 to equal 0', () => {
   expect(sub(10, 10)).toBe(0);
@@ -13,6 +13,3 @@ test('mult 10 * 10 to equal 100', () => {
   expect(mult(10, 10)).toBe(100);
 });
 
-test('div 10 / 10 to equal 1', () => {
-  expect(div(10, 10)).toBe(1);
-});
