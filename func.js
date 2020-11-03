61a01fc7d4a84cfce9710ec19617576d037bc793
Viktor Grigorev <7184243@gmail.com>
2020-11-03 22:57:18 +0300

    add func gendiff

--- func.js
+++ func.js
@@ -1,7 +1,12 @@
+import genDiff from '../backend-project-lvl2/gendiff.js';
+
 const add = (a, b) => a + b;
 const sub = (a, b) => a - b;
 const mult = (a, b) => a * b;
 const div = (a, b) => a / b;
+
+const diff = genDiff('/home/viktor/test2/f1.json', '/home/viktor/test2/f2.json');
+console.log(diff);
 
 export {
   add as default, sub, mult, div,
